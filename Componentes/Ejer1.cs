c40432e146c643e89dadd2d5b30993c2309dbade
Ero Sobrino <ero.sobrino.dorado@gmail.com>
2019-01-31 13:57:13 +0100

    31/1/19

---
+++ Componentes/Ejer1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Componentes
+{
+    public partial class Ejer1 : Control
+    {
+        [RefreshProperties(RefreshProperties.Repaint)]
+        public enum eMarca
+        {
+            Nada,
+            Cruz,
+            Circulo,
+            Imagen
+        }
+
+        private bool gradienteOpc = false;
+        [Category("Gradiente")]
+        [Description("Indica si aparece un gradiente de fondo")]
+        public bool GradienteOpc { get => gradienteOpc; set { gradienteOpc = value; this.Refresh(); } }
+
+        private Color gradientecolor1 = Color.Blue;
+        [Category("Gradiente")]
+        [Description("Indica el primer color del gradiente")]
+        public Color GradienteColor1 { get => gradientecolor1; set { gradientecolor1 = value; this.Refresh(); } }
+
+        private Color gradientecolor2 = Color.Red;
+        [Category("Gradiente")]
+        [Description("Indica el segundo color del gradiente")]
+        public Color GradienteColor2 { get => gradientecolor2; set { gradientecolor2 = value; this.Refresh(); } }
+
+        [Category("La propiedad cambió")]
+        [Description("Se lanza cuando se hace click sobre el")]
+
+        private Image imagenMarca;
+        [Category("Varios")]
+        [Description("Indica la ruta de la imagen de marca")]
+        public Image ImagenMarca { get => imagenMarca; set { imagenMarca = value; this.Refresh(); } }
+
+        private eMarca marca = eMarca.Nada;
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+            }
+            get { return marca; }
+        }
+
+
+        int grosor = 0; //Grosor de las líneas de dibujo
+        int offsetX = 0; //Desplazamiento a la derecha del texto
+
+        public Ejer1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            int offsetY = 0; //Desplazamiento hacia abajo del texto
+                             //Esta propiedad provoca mejoras en la apariencia o en la eficiencia
+                             // a la hora de dibujar
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            //Dependiendo del valor de la propiedad marca dibujamos una
+            //Cruz o un Círculo
+
+
+            if (gradientecolor1 != null && gradientecolor2 != null && GradienteOpc)
+            {
+                LinearGradientBrush gradiente = new LinearGradientBrush(new PointF(0, 0), new PointF(this.Width, this.Height), gradientecolor1, gradientecolor2);
+                g.FillRectangle(gradiente, 0, 0, this.Width, this.Height);
+            }
+
+            switch (Marca)
+            {
+                case eMarca.Circulo:
+                    grosor = 20;
+                    g.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor,
+                    this.Font.Height, this.Font.Height);
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor;
+                    break;
+                case eMarca.Cruz:
+                    grosor = 5;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+                    g.DrawLine(lapiz, grosor, grosor, this.Font.Height, this.Font.Height);
+                    g.DrawLine(lapiz, this.Font.Height, grosor, grosor, this.Font.Height);
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor / 2;
+                    //Es recomendable liberar recursos de dibujo pues se
+                    //pueden realizar muchos y cogen memoria
+                    lapiz.Dispose();
+                    break;
+                case eMarca.Imagen:
+                    if (imagenMarca != null)
+                    {
+                        g.DrawImage(imagenMarca, 0, 0, 50, 50);
+                        offsetX = 50;
+                        offsetY = 18;
+                    }
+                    break;
+            }
+            //Finalmente pintamos el Texto; desplazado si fuera necesario
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
+            Size tam = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY * 2);
+            b.Dispose();
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            this.Refresh();
+        }
+
+        private void Ejer1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.X < offsetX+grosor && Marca != eMarca.Nada)
+            {
+                ClickEnMarca?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        [Category("Acción")]
+        [Description("Se lanza cuando se hace click en la marca")]
+        public event System.EventHandler ClickEnMarca;
+    }
+}
---
+++ Componentes/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Componentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ejer11 = new Componentes.Ejer1();
+            this.SuspendLayout();
+            // 
+            // ejer11
+            // 
+            this.ejer11.GradienteColor1 = System.Drawing.Color.Blue;
+            this.ejer11.GradienteColor2 = System.Drawing.Color.Red;
+            this.ejer11.GradienteOpc = true;
+            this.ejer11.ImagenMarca = ((System.Drawing.Image)(resources.GetObject("ejer11.ImagenMarca")));
+            this.ejer11.Location = new System.Drawing.Point(448, 86);
+            this.ejer11.Marca = Componentes.Ejer1.eMarca.Imagen;
+            this.ejer11.Name = "ejer11";
+            this.ejer11.Size = new System.Drawing.Size(131, 52);
+            this.ejer11.TabIndex = 0;
+            this.ejer11.Text = "ejer11";
+            this.ejer11.ClickEnMarca += new System.EventHandler(this.ejer11_ClickEnMarca);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 707);
+            this.Controls.Add(this.ejer11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ejer1 ejer11;
+    }
+}
+
